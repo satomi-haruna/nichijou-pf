3329596e8ee4875fbc097ba692830e6e881f16dd
satomi-haruna <satomi.haruna1124@gmail.com>
2022-10-06 03:07:38 +0000

    [Add]homes/top,about [Update]public　ログイン,新規登録

--- db/schema.rb
+++ db/schema.rb
@@ -30,9 +30,14 @@ ActiveRecord::Schema.define(version: 2022_10_05_083658) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "nickname", null: false
+    t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
