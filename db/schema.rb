7133576084cb692d39ded4c647281c5350b54c1c
satomi-haruna <satomi.haruna1124@gmail.com>
2022-10-06 07:08:01 +0000

    [Add] schedule model,controller,routhing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_05_083658) do
+ActiveRecord::Schema.define(version: 2022_10_06_055609) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,18 @@ ActiveRecord::Schema.define(version: 2022_10_05_083658) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.string "plan"
+    t.datetime "plan_datetime"
+    t.boolean "allday_flg", default: false
+    t.text "memo"
+    t.string "place"
+    t.integer "color_id", default: 0
+    t.boolean "is_active", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
