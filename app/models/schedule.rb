be79a906da1d92ec397493518fcecab698601413
satomi-haruna <satomi.haruna1124@gmail.com>
2022-10-07 06:20:21 +0000

    [Add] color_id enum

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,18 @@
 class Schedule < ApplicationRecord
   belongs_to :user
 
+  # 色分けのためのcolor_idをenumで設定
+  enum color_id: {
+    colorless:    0,
+    pink:         1,
+    red:          2,
+    orange:       3,
+    yellow:       4,
+    yellow_green: 5,
+    green:        6,
+    light_blue:   7,
+    blue:         8,
+    purple:       9
+  }
+
 end
