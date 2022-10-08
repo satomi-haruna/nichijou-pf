80aa0a816bfb8161d063c8ce92479d10c0ab60c7
satomi-haruna <satomi.haruna1124@gmail.com>
2022-10-08 13:54:36 +0000

    [Add]user/show,edit

---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,31 @@
+class Public::UsersController < ApplicationController
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to users_my_page_path
+    else
+      render :edit
+    end
+  end
+
+  def unsubscribe
+  end
+
+  def withdraw
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :email, :is_deleted)
+  end
+
+end
