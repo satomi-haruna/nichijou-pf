a128cd69bdd1a1cd1e016ed491b10fe776cf59dd
satomi-haruna <satomi.haruna1124@gmail.com>
2022-10-10 16:23:24 +0000

    [Add]header link

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,10 @@
+class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!, if: :admin_url
+
+  def admin_url
+    request.fullpath.include?("/admin")
+  end
+
+  def index
+  end
+end
