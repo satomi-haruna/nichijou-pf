5a4279e2517e25f7ea661b8dd000b229f34d3d8a
satomi-haruna <satomi.haruna1124@gmail.com>
2022-10-11 02:10:03 +0000

    [Add]admin/users/index

--- db/migrate/20221008072613_create_lists.rb
+++ db/migrate/20221008072613_create_lists.rb
@@ -1,10 +1,10 @@
 class CreateLists < ActiveRecord::Migration[6.1]
   def change
     create_table :lists do |t|
-      t.integer :schedule_id
-      t.string :title
-      t.string :item
-      t.boolean :is_completed
+      t.integer :schedule_id, null: false
+      t.string :title, null: false
+      t.string :item, null: false
+      t.boolean :is_completed, null: false, default: false
 
       t.timestamps
     end
