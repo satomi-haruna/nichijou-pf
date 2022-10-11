3f91388ae6fb43019ee6655a84ae33ec8db8655d
satomi-haruna <satomi.haruna1124@gmail.com>
2022-10-11 10:04:23 +0000

    [Add]fullcalendar view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,47 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// インストールしたカレンダーのファイルを呼び出し
+import { Calendar} from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction';
+import monthGridPlugin from '@fullcalendar/daygrid'
+import googleCalendarApi from '@fullcalendar/google-calendar'
+
+document.addEventListener('turbolinks:load', function() {
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new Calendar(calendarEl, {
+    plugins: [ monthGridPlugin, interactionPlugin, googleCalendarApi ],
+
+    //細かな表示設定
+    locale: 'ja',
+    timeZone: 'Asia/Tokyo',
+    firstDay: 0,
+    headerToolbar: {
+      start: '',
+      center: 'title',
+      end: 'today prev,next'
+    },
+    expandRows: true,
+    stickyHeaderDates: true,
+    buttonText: {
+       today: '今日'
+    },
+    allDayText: '終日',
+    height: "auto",
+
+    dateClick: function(info){
+        //日付をクリックしたときのイベント(詳しくは次回の記事へ)
+    },
+    eventClick: function(info){
+        //表示されたイベントをクリックしたときのイベント(詳しくは次回の記事へ)
+    },
+    eventClassNames: function(arg){
+        //表示されたイベントにclassをcss用に追加する(詳しくは次回の記事へ)
+    }
+
+  });
+  //カレンダー表示
+  calendar.render();
+
+});
