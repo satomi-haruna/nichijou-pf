bbea973d419e83f4adad5baf966f7a87b2d130db
satomi-haruna <satomi.haruna1124@gmail.com>
2022-11-05 13:07:20 +0000

    [Fix]change_score migrate_file

--- db/migrate/20221105093637_change_score_to_diaries.rb
+++ db/migrate/20221105093637_change_score_to_diaries.rb
@@ -1,5 +1,6 @@
 class ChangeScoreToDiaries < ActiveRecord::Migration[6.1]
   def change
-    change_column :diaries, :score, :decimal, default: 0, null: false
+    change_column :diaries, :score, :decimal, precision: 5, scale: 3, default: 0, null: false
   end
+
 end
